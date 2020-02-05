7d957956d2d7e92616a47e15f106ba806754fa9e
DiaZzzFF <dvsh.dreamer@gmail.com>
2020-02-05 06:19:28 +0300

    Добавляет разметку + стили_1 (desktop) для comments

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+(function () {
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    freeMode: true,
+    slidesPerView: 3,
+    spaceBetween: 36,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.comments__arrow--next',
+      prevEl: '.comments__arrow--prev',
+    },
+  });
+
+})();
