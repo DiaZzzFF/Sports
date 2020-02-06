0e3257ff48358fbe1016678d8e8b47a92c0d28af
DiaZzzFF <dvsh.dreamer@gmail.com>
2020-02-06 04:09:12 +0300

    Добавляет стили_2 (mobile) для comments

--- source/js/script.js
+++ source/js/script.js
@@ -1,24 +1,64 @@
 'use strict';
 
 (function () {
-  var mySwiper = new Swiper ('.swiper-container', {
-    // Optional parameters
+  var currentBreakpoint = '';
+  var swiperComments;
+
+  var desktopSwiperConfig = {
     loop: true,
     freeMode: true,
     slidesPerView: 3,
     spaceBetween: 36,
-
-    // If we need pagination
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
     },
-
-    // Navigation arrows
     navigation: {
       nextEl: '.comments__arrow--next',
       prevEl: '.comments__arrow--prev',
     },
-  });
+  };
 
+  var mobileSwiperConfig = {
+    slidesPerView: 1,
+    centeredSlides: true,
+    loop: true,
+    freeMode: false,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: false,
+    },
+    navigation: {
+      nextEl: '.comments__arrow--next',
+      prevEl: '.comments__arrow--prev',
+    },
+  };
+
+  function initSwiper() {
+    if (window.matchMedia('(max-width: 767px)').matches && currentBreakpoint !== 'mobile') {
+      currentBreakpoint = 'mobile';
+
+      if (swiperComments) {
+        swiperComments.destroy();
+      }
+
+      swiperComments = new Swiper('.swiper-container', mobileSwiperConfig);
+      swiperComments.init();
+    }
+
+    if (window.matchMedia('(min-width: 768px)').matches && currentBreakpoint !== 'desktop') {
+      currentBreakpoint = 'desktop';
+
+      if (swiperComments) {
+        swiperComments.destroy();
+      }
+
+      swiperComments = new Swiper('.swiper-container', desktopSwiperConfig);
+      swiperComments.init();
+    }
+  }
+
+  initSwiper();
+
+  window.addEventListener('resize', initSwiper);
 })();
